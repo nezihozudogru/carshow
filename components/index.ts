706b562a3061e5f6412d9c93bfb8eef6aecc93df
nezihozudogru <nezih.ozudogru@gmail.com>
2024-04-02 16:02:48 +0300

    inital commit

---
+++ components/index.ts
@@ -0,0 +1,10 @@
+import Hero from './Hero';
+import customButton from './CustomButton';
+import Navbar from './Navbar';
+import SearchBar from './SearchBar';
+import CustomFilter from './CustomFilter';
+import SearchManufacture from './SearchManufacture';
+import CarCard from './CarCard';
+import CarDetail from './CardDetail';
+
+export { Hero, customButton, Navbar, CustomFilter, SearchBar, SearchManufacture, CarCard, CarDetail };
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = { images: { domains: ['cdn.imagin.studio'] } };
 
 export default nextConfig;
